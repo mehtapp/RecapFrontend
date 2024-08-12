bb4ffa01932d0672591da3ac8f7a25cc1c8b9b69
Mehtap Demir <mehtap.demir011@gmail.com>
2024-08-12 18:31:26 +0300

    template added and customer, brand, colour, car, rental components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColourComponent } from './components/colour/colour.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarComponent } from './components/car/car.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColourComponent,
-    BrandComponent
+    BrandComponent,
+    NavComponent,
+    CustomerComponent,
+    CarComponent,
+    RentalComponent
   ],
   imports: [
 
